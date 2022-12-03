78ad3b9f419e3e876e1957938b4ed671bf1a8bc7
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-03 18:05:41 -0300

    Lesson-2 -> Learning type basics.

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,13 @@
 var character = "luigi";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 30;
+var isBlackBelt = false;
+// character = 20; Error: Type 'number' is not assignable to type 'string'
+character = "mario";
+// age = "yoshi"; Error: Type 'number' is not assignable to type 'string'
+age = 20;
+// isBlackBelt = "yes"; Error: Type 'boolean' is not assignable to type 'string'
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(3));
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,19 @@
-const character = "luigi";
+let character: string = "luigi";
+let age = 30;
+let isBlackBelt = false;
 
-console.log(character);
+// character = 20; Error: Type 'number' is not assignable to type 'string'
+character = "mario";
 
-const inputs = document.querySelectorAll("input");
+// age = "yoshi"; Error: Type 'number' is not assignable to type 'string'
+age = 20;
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// isBlackBelt = "yes"; Error: Type 'boolean' is not assignable to type 'string'
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(3))
+
