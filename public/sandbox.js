e6709ddd77cb963ec7dd565268c5301894a6c6ec
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-03 21:23:21 -0300

    lesson-9 -> Learning about function signatures.

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,7 +1,20 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
+// Function signatures
+// let greet: Function;
+// ex1
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
+// ex2
+let calc;
+calc = (n1, n2, action) => {
+    if (action !== "add")
+        return n1 - n2;
+    return n1 + n2;
+};
+// ex3
+let logDetails;
+logDetails = (fighter) => {
+    console.log(`${fighter.name} is ${fighter.age} years old`);
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,9 +1,25 @@
-type StringOrNum = string | number;
-type ObjWithName = { name: string; uid: StringOrNum };
-const logDetails = (uid: StringOrNum, item: string) => {
-  console.log(`${item} has a uid of ${uid}`);
+// Function signatures
+// let greet: Function;
+
+// ex1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string): void => {
+  console.log(`${name} says ${greeting}`);
 };
 
-const greet = (user: ObjWithName) => {
-  console.log(`${user.name} says hello`);
+// ex2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (n1: number, n2: number, action: string) => {
+  if (action !== "add") return n1 - n2;
+  return n1 + n2;
+};
+
+// ex3
+let logDetails: (obj: { name: string; age: number }) => void;
+type Fighter = { name: string; age: number };
+
+logDetails = (fighter: Fighter) => {
+  console.log(`${fighter.name} is ${fighter.age} years old`);
 };
