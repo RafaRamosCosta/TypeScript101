ecef1f085e5679c55e4a73340c18495dcf3b9cfc
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-04 11:01:05 -0300

    Lesson-15 -> Learning about implementing interfaces in classes.

--- public/app.js
+++ public/app.js
@@ -1,28 +1,21 @@
-const me = {
-    name: "John",
-    age: 30,
-    speak(phrase) {
-        console.log(phrase);
-    },
-    spend(value) {
-        console.log("I spent ", value);
-        return value;
-    },
-};
-console.log(me);
-const greetPerson = (person) => {
-    console.log("hello", person.name);
-};
-greetPerson(me);
 import { Invoice } from "./classes/Invoice.js"; // it has to be the .js file because the browser only interprets javascript
-const invOne = new Invoice("Rafael", "work on the website", 400);
-const invTwo = new Invoice("Leo", "work on the API", 250);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Payment } from "./classes/Payment.js";
+let docOne;
+let docTwo;
+docOne = new Invoice("yoshi", "web work", 250);
+docTwo = new Payment("mario", "plumbing work", 300);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
+// const invOne = new Invoice("Rafael", "work on the website", 400);
+// const invTwo = new Invoice("Leo", "work on the API", 250);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 const form = document.querySelector(".new-item-form"); // uses the form type instead of the element type
 // inputs
 const type = document.querySelector("#type");
@@ -31,6 +24,11 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    // method to return value as a number
+    let doc;
+    if (type.value !== "invoice")
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    e;
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    console.log(doc);
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,30 @@
-// interfaces
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(phrase: string): void;
-  spend(value: number): number;
-}
-
-const me: IsPerson = {
-  name: "John",
-  age: 30,
-  speak(phrase: string): void {
-    console.log(phrase);
-  },
-  spend(value: number): number {
-    console.log("I spent ", value);
-    return value;
-  },
-};
-
-console.log(me);
-
-const greetPerson = (person: IsPerson) => {
-  console.log("hello", person.name);
-};
-
-greetPerson(me);
-
 import { Invoice } from "./classes/Invoice.js"; // it has to be the .js file because the browser only interprets javascript
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-const invOne = new Invoice("Rafael", "work on the website", 400);
-const invTwo = new Invoice("Leo", "work on the API", 250);
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+docOne = new Invoice("yoshi", "web work", 250);
+docTwo = new Payment("mario", "plumbing work", 300);
 
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.amount, inv.format());
-});
+let docs: HasFormatter[] = [];
+
+docs.push(docOne);
+docs.push(docTwo);
+
+console.log(docs);
+
+// const invOne = new Invoice("Rafael", "work on the website", 400);
+// const invTwo = new Invoice("Leo", "work on the API", 250);
+
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement; // uses the form type instead of the element type
 
@@ -52,6 +39,14 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
-  // method to return value as a number
+
+  let doc: HasFormatter;
+  if (type.value !== "invoice")
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+e
+  doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+
+  console.log(doc);
+
   console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
