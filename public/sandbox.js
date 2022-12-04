2a40a9604c51b8454269c788d7fbdb9a6bb2325a
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-03 21:04:40 -0300

    lesson-8 -> Learning about typing the return of functions.

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('oi');
+let greet; // typing a variable as a function
+// greet = "hello"; Error
+greet = () => {
+    console.log("Hello, world!");
+};
+const add = (n1, n2, n3) => {
+    console.log(n1 + n2);
+    console.log(n3);
+};
+add(5, 10);
+const minus = (n1, n2) => {
+    return n1 - n2;
+};
+let result = minus(10, 5);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,20 @@
-console.log('oi')
+let greet: Function; // typing a variable as a function
+
+// greet = "hello"; Error
+
+greet = (): void => {
+  console.log("Hello, world!");
+};
+
+const add = (n1: number, n2: number, n3?: number): void => {
+  console.log(n1 + n2);
+  console.log(n3);
+};
+
+add(5, 10);
+
+const minus = (n1: number, n2: number): number => {
+    return n1 - n2;
+}
+
+let result = minus(10, 5);
